fabdd9749a316193969d5ce365e08ec5f234f7d7
cbf <crazyblackfire@mail.ru>
2024-09-10 02:23:36 +0300

    change CStringConstructor to AAA

--- Lab1/test/test_class.cpp
+++ Lab1/test/test_class.cpp
@@ -10,20 +10,27 @@ TEST(ProjectTests, DefaultConstructor)
 TEST(ProjectTests, StdStringConstructor)
 {
     std::string test_str = "Hello";
+
     UpdatedString str(test_str);
+
     EXPECT_EQ(str.get(), "Hello");
 }
 
 TEST(ProjectTests, CStringConstructor)
 {
-    UpdatedString str("Hello");
+    const char* test_str = "Hello";
+
+    UpdatedString str(test_str);
+
     EXPECT_EQ(str.get(), "Hello");
 }
 
 TEST(ProjectTests, CopyConstructor)
 {
     UpdatedString original("Hello");
+
     UpdatedString copy(original);
+
     EXPECT_EQ(copy.get(), "Hello");
 }
 
@@ -31,14 +38,18 @@ TEST(ProjectTests, CopyAssignmentOperator)
 {
     UpdatedString original("Hello");
     UpdatedString copy;
+
     copy = original;
+
     EXPECT_EQ(copy.get(), "Hello");
 }
 
 TEST(ProjectTests, MoveConstructor)
 {
     UpdatedString original("Hello");
+
     UpdatedString moved(std::move(original));
+
     EXPECT_EQ(moved.get(), "Hello");
     EXPECT_EQ(original.get(), "");
 }
@@ -47,7 +58,9 @@ TEST(ProjectTests, MoveAssignmentOperator)
 {
     UpdatedString original("Hello");
     UpdatedString moved;
+
     moved = std::move(original);
+
     EXPECT_EQ(moved.get(), "Hello");
     EXPECT_EQ(original.get(), "");
 }
@@ -55,12 +68,15 @@ TEST(ProjectTests, MoveAssignmentOperator)
 TEST(ProjectTests, ClearMethod)
 {
     UpdatedString str("Hello");
+
     str.clear();
+
     EXPECT_EQ(str.get(), "");
 }
 
 TEST(ProjectTests, GetMethod)
 {
     UpdatedString str("Hello");
+    
     EXPECT_EQ(str.get(), "Hello");
 }
