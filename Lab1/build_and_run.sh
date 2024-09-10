69c7045d04d8a31ca9b81fde9bdf2de749975d85
cbf <crazyblackfire@mail.ru>
2024-09-10 21:18:46 +0300

    cleaned up --no-cache

--- Lab1/build_and_run.sh
+++ Lab1/build_and_run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-docker build -t docker-cpp-lab_1_tests -f Dockerfile --target lab_1_tests . --no-cache
-docker build -t docker-cpp-lab_1 -f Dockerfile --target lab_1 . --no-cache
+docker build -t docker-cpp-lab_1_tests -f Dockerfile --target lab_1_tests .
+docker build -t docker-cpp-lab_1 -f Dockerfile --target lab_1 .
 
 docker run docker-cpp-lab_1_tests
 docker run docker-cpp-lab_1
