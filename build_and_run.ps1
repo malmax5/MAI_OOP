c5062ee6dc68536a1148092e0afd6a6be61749b9
cbf <crazyblackfire@mail.ru>
2024-09-26 02:15:07 +0300

    build and run with cmake

--- build_and_run.ps1
+++ build_and_run.ps1
@@ -1,5 +1,6 @@
-docker build -t docker-cpp-lab_2_tests -f Dockerfile --target lab_2_tests .
-docker build -t docker-cpp-lab_2 -f Dockerfile --target lab_2 .
-
-docker run docker-cpp-lab_2_tests
-docker run docker-cpp-lab_2
+mkdir -p build
+cd build
+cmake -DCMAKE_BUILD_TYPE=Release ..
+cmake --build .
+./lab_2_tests
+cd ..
